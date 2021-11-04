60a4ca3bec99856036399ebe09dba2d9a855558a
Nicolas Biondini <nicolas.biondini@studio.unibo.it>
2021-11-04 16:57:47 +0100

    Added a function to read files froma graphical interface. Other minor fixes

--- Tests/Test.py
+++ Tests/Test.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
 
-
 import pytest
 import hypothesis.strategies as st
 from hypothesis import given, settings, assume
 from hypothesis import HealthCheck as HC
-
-
 
 
 from Neuroradiomics.registration import registration_reader
@@ -28,64 +25,46 @@ import itk
 def elastix_registration_strategy(draw):
     
     '''
-    This function generates two 2 or 3D blank itk images
+    This function generates a 3D blank itk image
     '''
     
-    Dimension = draw(st.integer(2,3))
+    Dimension = 3
     PixelType = itk.F
     ImageType = itk.Image[PixelType, Dimension]
         
         
-    Origin_fixed = draw(st.tuples(*[st.floats(0., 100.)] * Dimension))
-    Size_fixed = (draw(st.integers(10, 100)), draw(st.integers(10, 100)), draw(st.integers(10, 100)))
+    Origin = draw(st.tuples(*[st.integers(0., 100.)] * Dimension))
+    Size = (draw(st.integers(10, 100)), draw(st.integers(10, 100)), draw(st.integers(10, 100)))
     
-    Origin_moving = draw(st.tuples(*[st.floats(0., 100.)] * Dimension))
-    Size_moving = (draw(st.integers(10, 100)), draw(st.integers(10, 100)), draw(st.integers(10, 100)))
     
+    image = ImageType.New()
+    image = ImageType.New()
 
-    fixed_image = ImageType.New()
-    moving_image = ImageType.New()
+    region = itk.ImageRegion[Dimension]()
+    region.SetSize(Size)
+    region.SetIndex(Origin)
 
-    fixed_region = itk.ImageRegion[Dimension]()
-    fixed_region.SetSize(Size_fixed)
-    fixed_region.SetIndex(Origin_fixed)
+    image.SetRegions(region)
+    image.Allocate()
     
-    moving_region = itk.ImageRegion[Dimension]()
-    moving_region.SetSize(Size_moving)
-    moving_region.SetIndex(Origin_moving)
-
-    fixed_image.SetRegions(fixed_region)
-    fixed_image.Allocate()
     
-    moving_image.SetRegions(moving_region)
-    moving_image.Allocate()
-    
-    return fixed_image, moving_image
+    return image
 
 
 
 ################################################################################
-###                                                                          ###
 ###                                 TESTING                                  ###
-###                                                                          ###
 ################################################################################
 
-@given(elastix_registration_strategy())
-def elastix_registration_dimension_test(fixed_image, moving_image):
+@given(fixed_image = elastix_registration_strategy(), moving_image = elastix_registration_strategy())
+def test_elastix_registration_dimension(fixed_image, moving_image):
     
     '''
     This function tests if the final registered image has the same size of the initial fixed image
     '''
+    ImageType = itk.Image[itk.UC, 3]
+    registered_image = ImageType.New()
     registered_image, registration_parameters = elastix_registration(fixed_image, moving_image)
     assert (registered_image.GetSize() == fixed_image.GetSize())
     
-    
-
-
-
-
-
-
-
-
 
