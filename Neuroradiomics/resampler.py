2bec841c1d760fd28246ef4ca549103d7e2c9386
Nicolas Biondini <bionano94@gmail.com>
2023-03-07 09:50:29 +0100

    Added the Update function in the pipeline. Inserted the documentation

--- Neuroradiomics/resampler.py
+++ Neuroradiomics/resampler.py
@@ -4,6 +4,26 @@ __email__  = [ 'riccardo.biondi4@studio.unibo.it' ]
 import itk
 
 def match_physical_spaces(image, reference):
+    '''
+    This function matches the physical spaces of an image with another without modifying the image.
+    
+    Parameters
+    ----------
+            image: itk Image object.
+                   The image for which the physical spaces is intended to be change.
+                   
+            reference: itk Image object.
+                    The image which physical space will be matched.
+                    
+    Return
+    ------
+            resampler: itk Filter object.
+                    The filter used to match the physical space. 
+                    In order to obtain the matched image call the GetOutput() function to the output of this function.
+                    (e.g. resampler = match_physical_spaces(image, reference)
+                          new_image = resampler.GetOutput()
+                          )
+    '''
 
     NNInterpolatorType = itk.NearestNeighborInterpolateImageFunction[type(image),itk.D]
     
@@ -19,5 +39,6 @@ def match_physical_spaces(image, reference):
     _ = resampler.SetUseReferenceImage(True)
     _ = resampler.SetReferenceImage(reference)
     _ = resampler.SetInput(image)
+    _ = resampler.Update()
 
     return resampler
